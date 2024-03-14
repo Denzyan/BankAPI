8b4e3b0d72c4e0d845d2a67942cb7a8d4833b13d
Denzyan <striletsden@gmail.com>
2024-03-13 20:00:23 -0500

    Create_Project

---
+++ Controllers/Accounts.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Accounts : ControllerBase
+    {
+        [HttpGet]
+        public string GetAccount()
+        {
+            return "Acc N123, Name Scotty";
+        }
+
+    }
+}
